fac9e42b319ced9a4f17dd4135dd538e9bcf4140
ingBE <39978525+ingBE@users.noreply.github.com>
2018-06-07 00:12:26 +0900

    Update core.h

--- core.h
+++ core.h
@@ -16,21 +16,29 @@ namespace RWengine
             : x(x), y(y), z(z) {}
 
         void display() { std::cout << "( " << x << ", " << y << ", " << z << " )" << std::endl; }
+        
+        // 함수
         void invert()
         {
             x = -x;
             y = -y;
             z = -z;
         }
-
+        
         real squareMagnitude() const
         {
             return x*x + y*y + z*z;
         }
         
+        // 연산자  
         Vector3 operator+( Vector3 vector )
         {
             return Vector3( x + vector.x, y + vector.y, z + vector.z );
+        }
+        
+        Vector3 operator-( Vector3 vector )
+        {
+            return Vector3( x - vector.x, y - vector.y, z- vector.z );
         }
     };
 }
