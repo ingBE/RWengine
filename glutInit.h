5b52f5332c77534926584f72e375983f414cd3c4
ingBE <39978525+ingBE@users.noreply.github.com>
2018-07-02 03:05:08 +0900

    Add files via upload

--- glutInit.h
+++ glutInit.h
@@ -1,7 +1,11 @@
 #include <GL/glut.h>
 
+int w, h;
+
 void glChangeSize(int width, int height)
 {
+    w = width; h = height;
+
     // 0으로 나누지 못함
     if (height == 0) { height = 1; }
     float ratio = 1.0 * width / height;
@@ -25,6 +29,11 @@ void glutInit( int argc, char **argv)
     glutInitWindowPosition( 100, 30 );
     glutInitWindowSize( 1024, 700 );
     glutCreateWindow( "R A I N W A T E R" );
+}
+
+void exitKey(unsigned char key, int x, int y)
+{
+    if (key == 27) { exit(0); }
 }
 
 void glCreateCoordinate( float y, float z, float x )
@@ -57,4 +66,67 @@ void glCreateGround( float x, float y )
         glVertex3f( y, 0.0, x );
         glVertex3f( y, 0.0, -x);
     glEnd();
+}
+
+void setOrthographicProjection()
+{
+    // 행렬을 투영모드로 변경합니다.
+    glMatrixMode(GL_PROJECTION);
+    // 원근투영에 대한 환경변수를 
+    // 가지고 있는 이전 행렬을 저장합니다.
+    glPushMatrix();
+        // 행렬을 초기화합니다.
+        glLoadIdentity();
+        // 2D 직교투영을 설정합니다.
+        gluOrtho2D(0, w, 0, h);
+        // y 축을 뒤집습니다.
+        glScalef(1, -1, 1);
+        // 좌측아래의 원점을 
+        // 좌측위로 옮깁니다.
+        glTranslatef(0, -h, 0);
+    glMatrixMode(GL_MODELVIEW);
+}
+
+void resetPerspectiveProjection()
+{
+    glMatrixMode(GL_PROJECTION);
+    glPopMatrix();
+    glMatrixMode(GL_MODELVIEW);
+}
+
+void renderBitmapString( float x, float y, void *font, char *string)
+{
+    char *c;
+    int x1=x;
+    for (c=string; *c != '\0'; c++)
+    {
+        glRasterPos2f(x1,y);
+        glutBitmapCharacter(font, *c);
+        x1 = x1 + glutBitmapWidth(font,*c);
+    }
+}
+
+int frame, time, timebase=0;
+char s[60];
+
+void fps()
+{
+
+    frame++;
+    time=glutGet(GLUT_ELAPSED_TIME);
+    if (time - timebase > 1000)
+    {
+        sprintf(s,"FPS:%4.2f",
+        frame*1000.0/(time-timebase));
+        timebase = time; 
+        frame = 0;
+    }
+
+    glColor3f(0.0f,1.0f,1.0f);
+    glPushMatrix();
+        glLoadIdentity();
+        setOrthographicProjection();
+        renderBitmapString(30,35,GLUT_BITMAP_9_BY_15,s);
+    glPopMatrix();
+    resetPerspectiveProjection();
 }
