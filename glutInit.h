0958e4f10ab70d2748776034c3ee78de54421cb5
ingBE <39978525+ingBE@users.noreply.github.com>
2018-07-01 18:08:13 +0900

    Add files via upload

--- glutInit.h
+++ glutInit.h
@@ -1,6 +1,6 @@
 #include <GL/glut.h>
 
-void changeSize(int width, int height)
+void glChangeSize(int width, int height)
 {
     // 0으로 나누지 못함
     if (height == 0) { height = 1; }
@@ -16,7 +16,6 @@ void changeSize(int width, int height)
     gluPerspective(45, ratio, 1, 1000);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    gluLookAt(0.0,0.0,5.0, 0.0,0.0,-1.0, 0.0,1.0,0.0);
 }
 
 void glutInit( int argc, char **argv)
@@ -26,5 +25,36 @@ void glutInit( int argc, char **argv)
     glutInitWindowPosition( 100, 30 );
     glutInitWindowSize( 1024, 700 );
     glutCreateWindow( "R A I N W A T E R" );
-    glutReshapeFunc(changeSize);
+}
+
+void glCreateCoordinate( float y, float z, float x )
+{
+    glColor3f( 0.0, 1.0, 0.0 );
+    glBegin(GL_LINE_STRIP);
+        glVertex3f( -y, 0.0, 0.0 );
+        glVertex3f( y, 0.0, 0.0 );
+    glEnd();
+
+    glColor3f( 0.0, 1.0, 1.0 );
+    glBegin(GL_LINE_STRIP);
+        glVertex3f( 0.0, -z, 0.0 );
+        glVertex3f( 0.0, z, 0.0 );
+    glEnd();
+
+    glColor3f( 1.0, 0.0, 1.0 );
+    glBegin(GL_LINE_STRIP);
+        glVertex3f( 0.0, 0.0, -x );
+        glVertex3f( 0.0, 0.0, x );
+    glEnd();
+}
+
+void glCreateGround( float x, float y )
+{
+    glColor3f(0.9, 0.9, 0.9);
+    glBegin(GL_QUADS);
+        glVertex3f(-y, 0.0, -x);
+        glVertex3f(-y, 0.0, x );
+        glVertex3f( y, 0.0, x );
+        glVertex3f( y, 0.0, -x);
+    glEnd();
 }
--- gravity.cpp
+++ gravity.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <assert.h>
-#include <ctime>
 #include "glutInit.h"
 #include "precision.h"
 #include "core.h"
@@ -9,17 +8,26 @@
 
 using namespace RWengine;
 
+// 카메라의 위치
+float x = 5, y = -2, z = 1;
+
+// 카메라의 시선
+float lx = -6, ly = 5, lz = 0;
+
 Particle particle;
 Vector3 position;
-Vector3 gravity(0,0,-1.0);
+Vector3 gravity( 0, 0, -9.8 );
 real duration;
-clock_t start = 0, end = 0;
+
+void changeSize(int width, int height)
+{
+    glChangeSize( width, height );
+    gluLookAt( y, z, x, y+ly, z+lz, x+lx, 0.0,1.0,0.0);
+}
 
 void renderScene(void)
 {
-//    end = clock();
-//    duration = ((real)(end - start))/(CLOCKS_PER_SEC*1000.0);
-    duration = 0.01;
+    duration = 0.005;
 
     position = particle.getPosition();
     particle.addForce( gravity );
@@ -27,31 +35,46 @@ void renderScene(void)
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glEnable(GL_BLEND);
     glEnable(GL_POINT_SMOOTH);
+    glEnable(GL_LINE_SMOOTH);
+    glEnable(GL_POLYGON_SMOOTH);
+
     glPushMatrix();
 
-    glColor3f( 1.0, 1.0, 1.0 );
-    glPointSize( 10.0 );
-   
-    glBegin(GL_POINTS);
-        glVertex3f( position.y, position.z, position.x );
-    glEnd();
+    glCreateCoordinate( 100.0, 100.0, 100.0 );
+    glCreateGround( 100, 100 );
+
+    glColor3f( 1.0, 0.5, 0.0 );
+
+    glTranslatef( position.y, position.z, position.x );
+    glutSolidSphere( 0.05, 50, 50 );
 
     glPopMatrix();
+
     glutSwapBuffers();
 
     particle.integrate( duration );
-
-//    start = clock();
 }
 
 void startKey(unsigned char key, int x, int y)
 {
     switch(key)
     {
+        case '1':
+            x = 5, y = -2, z = 1;
+            lx = -6, ly = 5, lz = 0;
+            glLoadIdentity();
+            gluLookAt(y,z,x,y+ly,z+lz,x+lx,0.0,1.0,0.0);
+            break;
+        case '2':
+            x = 5, y = 5, z = 1;
+            lx = -6, ly = -2, lz = 0;
+            glLoadIdentity();
+            gluLookAt(y,z,x,y+ly,z+lz,x+lx,0.0,1.0,0.0);
+            break;
         case 'A':
         case 'a':
-            particle.setPosition( 0.0, -2.0, -1.0 );
-            particle.setVelocity( 0.0, 1.0, 2.3 );
+            particle.setPosition( 0.0, 0.0, 0.0 );
+            particle.setVelocity( -3.0, 3.5, 7.0 );
             break;
     }
 }
@@ -62,6 +85,7 @@ int main(int argc, char **argv )
     particle.setDamping( 0.0 );
 
     glutInit( argc, argv );
+    glutReshapeFunc(changeSize);
     glutDisplayFunc(renderScene); 
 
     glutIdleFunc(renderScene);
